b1f99b76b7f4757022f68afe0e94e2274b509244
网商念 <88571646+wsn888@users.noreply.github.com>
2021-08-07 15:09:41 +0800

    Update bs.py

--- bs.py
+++ bs.py
@@ -7,8 +7,11 @@ number2 = random.randint(9857,9857)
 dict = {
         '账号':'密码'
         }
-        
-header={
+
+* 使用说明：
+* 使用前，手机注册小米运动账号，设置第三方接入，然后到网页提交步数即可
+* 隐私说明：本站保障您的隐私安全，请勿设置和其他软件同样密码，建议简单密码即可
+* 注意不要使用小米账号登录，使用手机号重新注册，可以和小米一个手机号，是两个独立的系统
 
     'User_Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)',
 
