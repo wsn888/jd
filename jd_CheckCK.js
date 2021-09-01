9497c7c4ab1313befb65b851d93d2207562a6d13
网商念 <88571646+wsn888@users.noreply.github.com>
2021-09-01 17:32:26 +0800

    Update jd_CheckCK.js

--- jd_CheckCK.js
+++ jd_CheckCK.js
@@ -5,7 +5,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const got = require('got');
-const {getEnvs,DisableCk,EnableCk} = require('./ql');
+const {getEnvs,DisableCk,EnableCk} = require('./QL');
 const api = got.extend({
   retry: { limit: 0 },
   responseType: 'json',
