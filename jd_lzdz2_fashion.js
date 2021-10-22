7d6ba2d19095cc88456e886ef4d1702fcbf36eb4
网商念 <88571646+wsn888@users.noreply.github.com>
2021-10-23 00:29:13 +0800

    Update jd_lzdz2_fashion.js

--- jd_lzdz2_fashion.js
+++ jd_lzdz2_fashion.js
@@ -1,6 +1,6 @@
 /*
 时尚宠粉趴
-https://lzdz2-isv.isvjcloud.com/dingzhi/fashion/recruit/activity/13145?activityId=1ad06f0cb93e4928a894e3b984c2fa4b
+https://lzdz2-isv.isvjcloud.com/dingzhi/fashion/recruit/activity/13145?activityId=33bb040e182d4e998065c0276d19c529
 **/
 
 const $ = new Env("超店会员福利社");
@@ -31,7 +31,7 @@ if ($.isNode()) {
     authorCodeList = await getAuthorCodeList('https://gitee.com/fatelight/dongge/raw/master/dongge/lzdz2_fashion.json')
     if(authorCodeList === '404: Not Found'){
         authorCodeList = [
-            'b3b3f59c9015493a9d8e2f1d5d9b92ea',
+            '33bb040e182d4e998065c0276d19c529',
         ]
     }
     for (let i = 0; i < cookiesArr.length; i++) {
