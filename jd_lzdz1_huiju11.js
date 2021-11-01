a5b0db36406a5fd9bd283f72a3cc2165a9be8398
网商念 <88571646+wsn888@users.noreply.github.com>
2021-11-01 15:22:56 +0800

    Add files via upload

--- jd_lzdz1_huiju11.js
+++ jd_lzdz1_huiju11.js
@@ -1,7 +1,7 @@
 /*
 惠聚11.11  好物乐享不停
 11.1 - 11.12
-https://lzdz1-isv.isvjcloud.com/dingzhi/dz/openCard/activity?activityId=dzlbklzmsk20211101A&shareUuid=7b75b5a8b26d4bca9f94d794c5a1965b
+https://lzdz1-isv.isvjcloud.com/dingzhi/dz/openCard/activity?activityId=dzlbklzmsk20211101A&shareUuid=b56485541148443bb2831bf6425645d5
 
 默认执行脚本。如果需要不执行
 环境变量 NO_RUSH=false
@@ -38,7 +38,7 @@ if (process.env.NO_RUSH && process.env.NO_RUSH != "") {
     authorCodeList = await getAuthorCodeList('https://gitee.com/fatelight/dongge/raw/master/dongge/lzdz1_huanju11.json')
     if(authorCodeList === '404: Not Found'){
         authorCodeList = [
-            '7b75b5a8b26d4bca9f94d794c5a1965b',
+            'b56485541148443bb2831bf6425645d5',
         ]
     }
 
@@ -70,7 +70,7 @@ if (process.env.NO_RUSH && process.env.NO_RUSH != "") {
             $.activityId = 'dzlbklzmsk20211101A'
             $.activityShopId = '1000004489'
             $.activityUrl = `https://lzdz1-isv.isvjd.com/dingzhi/dz/openCard/activity/${$.authorNum}?activityId=${$.activityId}&shareUuid=${encodeURIComponent($.authorCode)}&adsource=null&shareuserid4minipg=null&shopid=${$.activityShopId}&lng=00.000000&lat=00.000000&sid=&un_area=`
-            if (isRush) {
+            if (isRush === true) {
                 console.log("未检测到不执行环境变量，执行任务")
                 await rush();
             } else {
