15b24e5af5d0031fb11f5d84171bf9c3c579b27f
网商念 <88571646+wsn888@users.noreply.github.com>
2021-11-17 23:54:44 +0800

    Update jd_fcwb_wb.js

--- jd_fcwb_wb.js
+++ jd_fcwb_wb.js
@@ -1,4 +1,4 @@
-const $ = new Env('Faker群友改的发财挖宝');
+const $ = new Env('群友改的发财挖宝');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
