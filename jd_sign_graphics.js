a733c7e2f8d6be90ec9ec303139875b128c51b26
网商念 <88571646+wsn888@users.noreply.github.com>
2021-12-15 12:47:58 +0800

    Add files via upload

--- jd_sign_graphics.js
+++ jd_sign_graphics.js
@@ -9,7 +9,7 @@ npm i png-js 或者 npm i png-js -S
 修改域名 https://jdjoy.jd.com 可以改成ip https://49.7.27.236
 */
 
-const validator = require('./JDJRValidator_Smiek.js');
+const validator = require('./JDJRValidator_Pure.js');
 const Faker=require('./sign_graphics_validate.js') 
 
 const $ = new Env('京东签到图形验证');
@@ -136,7 +136,7 @@ function Login(i) {
                 $.appId = 'b342e';
                 await requestAlgo();
                 await Sign(i,1)
-                let time = Math.random() * 5000 + 10000
+                let time = Math.random() * 5000 + 15000
                 console.log(`等待${(time/1000).toFixed(3)}秒`)
                 await $.wait(parseInt(time, 10))
               } else if (data.hasSign === true) {
