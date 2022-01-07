c09265e75d5c5d44d8b637ce8d6ff75e299de819
网商念 <88571646+wsn888@users.noreply.github.com>
2022-01-07 09:43:32 +0800

    Update jd_opencardLH37.js

--- jd_opencardLH37.js
+++ jd_opencardLH37.js
@@ -48,7 +48,7 @@ let activityCookie =''
   $.userId = '10299171'
   $.actId = 'c49b9bf1e3c3487dac8ab_22010601'
   $.MixNicks = ''
-  $.inviteNick = 'k1Nobb+P0er+C2sysxnx/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ/e9r/uf'
+  $.inviteNick = 'QC6MjIz9kcaCtoRUgFAh1V4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w=='
   console.log(`\n活动地址:https://jinggengjcq-isv.isvjcloud.com/fronth5/#/pages/unitedCardNew20220106/unitedCardNew20220106?actId=c49b9bf1e3c3487dac8ab_22010601&bizExtString=c2hhcmVOaWNrOmsxTm9iYiUyQlAwZXIlMkJDMnN5c3hueCUyRlAyS0VMTzlpelJWcHdDeXF1MGVxVlo1YVc3Ukh6bE1vYnJ6SiUyRmU5ciUyRnVm`)
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
