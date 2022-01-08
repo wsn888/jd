9fdbead350e251d3f055c72ccd0717eb326936d1
网商念 <88571646+wsn888@users.noreply.github.com>
2022-01-08 12:04:19 +0800

    Update gua_opencard97.js

--- gua_opencard97.js
+++ gua_opencard97.js
@@ -25,7 +25,7 @@ guaopenwait97="0"
 
 All变量适用
 ————————————————
-入口：[ 1.8~1.27 集年味卡 享年味盛典 (https://lzdz1-isv.isvjcloud.com/dingzhi/customized/common/activity?activityId=cd20220108iu890dmnhimj01ied&shareUuid=977d4b9a6f1049f0b4ec6b658fe3fd33)]
+入口：[ 1.8~1.27 集年味卡 享年味盛典 (https://lzdz1-isv.isvjcloud.com/dingzhi/customized/common/activity?activityId=cd20220108iu890dmnhimj01ied&shareUuid=38fa451f7ac64e20839f9234906d49ce)]
 
 请求太频繁会被黑ip
 过10分钟再执行
@@ -105,7 +105,7 @@ let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*
     return;
   }
   $.activityId = "cd20220108iu890dmnhimj01ied"
-  $.shareUuid = "977d4b9a6f1049f0b4ec6b658fe3fd33"
+  $.shareUuid = "38fa451f7ac64e20839f9234906d49ce"
   console.log(`入口:\nhttps://lzdz1-isv.isvjcloud.com/dingzhi/customized/common/activity?activityId=${$.activityId}&shareUuid=${$.shareUuid}`)
 
   for (let i = 0; i < cookiesArr.length; i++) {
