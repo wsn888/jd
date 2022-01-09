ec6c3ebb9ad0ba09e55ebf0a6acdb610a4640ea1
网商念 <88571646+wsn888@users.noreply.github.com>
2022-01-09 22:18:17 +0800

    Update jd_nhRed.js

--- jd_nhRed.js
+++ jd_nhRed.js
@@ -19,7 +19,7 @@ cron "0 0,23,12 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJ
 const $ = new Env('年货节火力值');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
-let rebateCodes = 'SKib2Yf'
+let rebateCodes = 'SdiaKDy'
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [],
     cookie = '';
