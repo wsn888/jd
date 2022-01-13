d9c1ca8b6863766b5e7fff7b26ea432d8b6d3a8a
网商念 <88571646+wsn888@users.noreply.github.com>
2022-01-13 21:00:42 +0800

    Add files via upload

--- gua_opencard104.js
+++ gua_opencard104.js
@@ -1,16 +1,16 @@
 /*
-1.13~1.18 联合开卡-103 [gua_opencard103.js]
+1.6~1.14 联合开卡-104 [gua_opencard104.js]
 新增开卡脚本
 一次性脚本
 
-1.邀请一人5豆(有可能没有豆
-2.开12张 成功开1张 可能获得5京豆
+1.邀请一人20豆(有可能没有豆
+2.开6张 成功开1张 可能获得5京豆
   全部开完获得1次抽奖
 3.关注10京豆 获得1次抽奖
-4.加购5京豆 获得1次抽奖(默认不加购 如需加购请设置环境变量[guaopencard_addSku103]为"true"
-5.抽奖 (默认不抽奖 如需抽奖请设置环境变量[guaopencard_draw103]为"3"
+4.加购5京豆 获得1次抽奖(默认不加购 如需加购请设置环境变量[guaopencard_addSku104]为"true"
+5.抽奖 (默认不抽奖 如需抽奖请设置环境变量[guaopencard_draw104]为"3"
 填写要抽奖的次数 不足已自身次数为准
-guaopencard_draw103="3"
+guaopencard_draw104="3"
 填非数字会全都抽奖
 
 第一个账号助力作者 其他依次助力CK1
@@ -18,25 +18,25 @@ guaopencard_draw103="3"
 
 默认脚本不执行
 如需执行脚本请设置环境变量
-guaopencard103="true"
+guaopencard104="true"
 每个账号之间延迟 100=延迟100秒 0=延迟0秒会使用每3个账号延迟60秒
 guaopenwait_All 所有
-guaopenwait103="0"
+guaopenwait104="0"
 
 
 All变量适用
 ————————————————
-入口：[ 1.13~1.18 联合开卡-103 (https://3.cn/10-4FafAy)]
+入口：[ 1.6~1.14 联合开卡-104 (https://3.cn/104R8-Db7)]
 
 请求太频繁会被黑ip
 过10分钟再执行
 
 
-cron:30 9 13-18/3 1 *
+cron:20 0 6-14 1 *
 ============Quantumultx===============
 [task_local]
-#1.13~1.18 联合开卡-103
-30 3 13-18/3 1 * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_opencard103.js, tag=1.13~1.18 联合开卡-103, enabled=true
+#1.6~1.14 联合开卡-104
+20 0 6-14 1 * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_opencard104.js, tag=1.6~1.14 联合开卡-104, enabled=true
 
 */
 let guaopencard_addSku = "false"
@@ -44,7 +44,7 @@ let guaopencard = "false"
 let guaopenwait = "0"
 let guaopencard_draw = "0"
 
-const $ = new Env('1.13~1.18 联合开卡-103');
+const $ = new Env('1.6~1.14 联合开卡-104');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cleanCart = ''
@@ -69,14 +69,14 @@ if ($.isNode()) {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 
-guaopencard_addSku = $.isNode() ? (process.env.guaopencard_addSku103 ? process.env.guaopencard_addSku103 : `${guaopencard_addSku}`) : ($.getdata('guaopencard_addSku103') ? $.getdata('guaopencard_addSku103') : `${guaopencard_addSku}`);
+guaopencard_addSku = $.isNode() ? (process.env.guaopencard_addSku104 ? process.env.guaopencard_addSku104 : `${guaopencard_addSku}`) : ($.getdata('guaopencard_addSku104') ? $.getdata('guaopencard_addSku104') : `${guaopencard_addSku}`);
 guaopencard_addSku = $.isNode() ? (process.env.guaopencard_addSku_All ? process.env.guaopencard_addSku_All : `${guaopencard_addSku}`) : ($.getdata('guaopencard_addSku_All') ? $.getdata('guaopencard_addSku_All') : `${guaopencard_addSku}`);
-guaopencard = $.isNode() ? (process.env.guaopencard103 ? process.env.guaopencard103 : `${guaopencard}`) : ($.getdata('guaopencard103') ? $.getdata('guaopencard103') : `${guaopencard}`);
+guaopencard = $.isNode() ? (process.env.guaopencard104 ? process.env.guaopencard104 : `${guaopencard}`) : ($.getdata('guaopencard104') ? $.getdata('guaopencard104') : `${guaopencard}`);
 guaopencard = $.isNode() ? (process.env.guaopencard_All ? process.env.guaopencard_All : `${guaopencard}`) : ($.getdata('guaopencard_All') ? $.getdata('guaopencard_All') : `${guaopencard}`);
-guaopenwait = $.isNode() ? (process.env.guaopenwait103 ? process.env.guaopenwait103 : `${guaopenwait}`) : ($.getdata('guaopenwait103') ? $.getdata('guaopenwait103') : `${guaopenwait}`);
+guaopenwait = $.isNode() ? (process.env.guaopenwait104 ? process.env.guaopenwait104 : `${guaopenwait}`) : ($.getdata('guaopenwait104') ? $.getdata('guaopenwait104') : `${guaopenwait}`);
 guaopenwait = $.isNode() ? (process.env.guaopenwait_All ? process.env.guaopenwait_All : `${guaopenwait}`) : ($.getdata('guaopenwait_All') ? $.getdata('guaopenwait_All') : `${guaopenwait}`);
 guaopenwait = parseInt(guaopenwait, 10) || 0
-guaopencard_draw = $.isNode() ? (process.env.guaopencard_draw103 ? process.env.guaopencard_draw103 : guaopencard_draw) : ($.getdata('guaopencard_draw103') ? $.getdata('guaopencard_draw103') : guaopencard_draw);
+guaopencard_draw = $.isNode() ? (process.env.guaopencard_draw104 ? process.env.guaopencard_draw104 : guaopencard_draw) : ($.getdata('guaopencard_draw104') ? $.getdata('guaopencard_draw104') : guaopencard_draw);
 guaopencard_draw = $.isNode() ? (process.env.guaopencard_draw ? process.env.guaopencard_draw : guaopencard_draw) : ($.getdata('guaopencard_draw') ? $.getdata('guaopencard_draw') : guaopencard_draw);
 guaopenwait = parseInt(guaopenwait, 10) || 0
 allMessage = ""
@@ -89,7 +89,7 @@ let activityCookie =''
 !(async () => {
   if ($.isNode()) {
     if(guaopencard+"" != "true"){
-      console.log('如需执行脚本请设置环境变量[guaopencard103]为"true"')
+      console.log('如需执行脚本请设置环境变量[guaopencard104]为"true"')
     }
     if(guaopencard+"" != "true"){
       return
@@ -104,10 +104,11 @@ let activityCookie =''
   // return
   $.appkey = '51B59BB805903DA4CE513D29EC448375'
   $.userId = '10299171'
-  $.actId = 'f6b9469f2ce7439a99bb47_220113'
+  $.actId = 'c49b9bf1e3c3487dac8ab_22010601'
   $.MixNicks = ''
   $.inviteNick = 'QC6MjIz9kcaCtoRUgFAh1V4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w=='
-  console.log(`活动地址:https://3.cn/10-4FafAy`)
+  console.log(`活动地址:https://3.cn/104R8-Db7`)
+
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
     if (cookie) {
@@ -210,7 +211,7 @@ async function run() {
         }
       }
     }else{
-      console.log('如需加购请设置环境变量[guaopencard_addSku103]为"true"');
+      console.log('如需加购请设置环境变量[guaopencard_addSku104]为"true"');
     }
     await takePostRequest('activity_load');
     if(guaopencard_draw+"" !== "0"){
@@ -230,7 +231,7 @@ async function run() {
         }
         await $.wait(parseInt(Math.random() * 2000 + 2000, 10))
       }
-    }else console.log('如需抽奖请设置环境变量[guaopencard_draw103]为"3" 3为次数');
+    }else console.log('如需抽奖请设置环境变量[guaopencard_draw104]为"3" 3为次数');
     await takePostRequest('myAward');
     await takePostRequest('missionInviteList');
     console.log($.MixNick)
@@ -418,9 +419,15 @@ async function dealReturn(type, data) {
                 let value = 0
                 for(let i in res.data.list || []){
                   let item = res.data.list[i]
-                  value += Number(item.awardDes)
+                  if(item.awardDes == '20'){
+                    num++
+                    value = item.awardDes
+                  }else{
+                    if(type == "myAward") console.log(`${item.awardName}`)
+                  }
                 }
-                if(value > 0) console.log(`共获得${value}京豆\n无法判断奖励是否为邀请奖励，所以直接显示获得多少豆\n`)
+                if($.index == 1 || type == "myAwards") $.MAcount = num
+                if(num > 0 && type == "myAward") console.log(`邀请好友(${num}):${num*parseInt(value, 10) || 30}京豆`)
               }else if(type == "missionInviteList"){
                 console.log(`邀请人数(${res.data.invitedLogList.total})`)
               }
