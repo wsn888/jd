6a627562d43560408ccade03f93afe35ed7866ca
网商念 <88571646+wsn888@users.noreply.github.com>
2022-01-16 16:37:11 +0800

    更新部分脚本

--- jd_tiger.js
+++ jd_tiger.js
@@ -1,8 +1,8 @@
 /*
 萌虎摇摇乐
 https://yearfestival.jd.com
-优先内部互助,剩余次数助力作者
-1 0,12,18 * * * jd_tiger.js
+优先内部互助,剩余次数助力作者和助力池
+0 0,12,18 * * * jd_tiger.js
 转义自HW大佬
 const $ = new Env('萌虎摇摇乐');
 */
@@ -76,13 +76,14 @@ Object.keys(jdCookieNode).forEach((item) => {
     for (let i = 0; i < cookiesArr.length; i++) {
         cookie = cookiesArr[i]
         const userName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
+        const pool = await getShareCodePool('tiger', 5)
         // if (shareCodesHW.length === 0) {
         //     shareCodesHW = await getshareCodeHW('tiger')
         // }
         // index === 0 ?
         //     shareCodes = Array.from(new Set([...shareCodesHW, ...shareCodesSelf, ...temp])) :
         //     shareCodes = Array.from(new Set([...shareCodesSelf, ...shareCodesHW, ...temp]))
-        shareCodes = Array.from(new Set([...shareCodesSelf, ...authorCode]))
+        shareCodes = Array.from(new Set([...shareCodesSelf, ...authorCode, ...pool]))
         // console.log(shareCodes)
         for (let code of shareCodes) {
             console.log(`账号${i + 1} 去助力 ${code} ${shareCodesSelf.includes(code) ? '(内部)' : ''}`)
@@ -178,6 +179,27 @@ async function api(r_body) {
     // console.log(body)
     return JSON.parse(body)
 }
+
+async function getShareCodePool(key, num) {
+    let shareCode = []
+    for (let i = 0; i < 2; i++) {
+        try {
+            const { body } = await got(`https://api.jdsharecode.xyz/api/${key}/${num}`)
+            console.debug('getShareCodePool:', body)
+            shareCode = JSON.parse(body).data || []
+            console.log(`随机获取${num}个${key}成功：${JSON.stringify(shareCode)}`)
+            if (shareCode.length !== 0) {
+                break
+            }
+        } catch (e) {
+            // console.warn(e.stack)
+            console.log("getShareCodePool Error, Retry...")
+            await wait(2000 + Math.floor((Math.random() * 4000)))
+        }
+    }
+    return shareCode
+}
+
 
 async function getTaskDetail(taskGroupId) {
     let res = await api({ "taskGroupId": taskGroupId, "apiMapping": "/api/task/brand/getTaskList" })
