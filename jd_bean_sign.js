d47eada007fec750134683d67e2c7c02b94974c5
网商念 <88571646+wsn888@users.noreply.github.com>
2022-03-06 20:38:30 +0800

    更新

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -3,9 +3,10 @@
 活动入口：各处的签到汇总
 Node.JS专用
 5 0,17 * * * jd_bean_sign.js
+IOS软件用户请使用 https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
 更新时间：2021-6-18
-推送通知默认简洁模式(多账号只发送一次)。
-如需详细通知，设置环境变量 JD_BEAN_SIGN_NOTIFY_SIMPLE 为false即可(N账号推送N次通知)。
+推送通知默认简洁模式(多账号只发送一次)。如需详细通知，设置环境变量 JD_BEAN_SIGN_NOTIFY_SIMPLE 为false即可(N账号推送N次通知)。
+Modified From github https://github.com/ruicky/jd_sign_bot
  */
 const $ = new Env('京东多合一签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
