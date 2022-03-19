df9bd43a646d39ab2ca6fa436215373798eb0a18
网商念 <88571646+wsn888@users.noreply.github.com>
2022-03-19 19:52:09 +0800

    更新

--- jd_indeps.sh
+++ jd_indeps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #依赖安装，运行一次就好
-#0 8 1 1 * jd_indeps.sh
+#0 jd_indeps.sh
 #const $ = new Env('依赖安装');
 #
 echo "安装脚本所需依赖，首次拉库运行一次即可"
@@ -17,6 +17,7 @@ pnpm install -g @types/node
 pnpm install -g request
 pnpm install -g jsdom
 pnpm install -g moment
+pnpm install -g tough-cookie
 pip install jieba
 
 echo "恭喜，已全部安装完成!"
--- m_jd_wx_collectCard.js
+++ m_jd_wx_collectCard.js
@@ -1,9 +1,8 @@
-//问题反馈:https://t.me/Wall_E_Channel
 let mode = __dirname.includes('magic')
-const {Env} = mode ? require('./magic') : require('./magic')
+//const {Env} = mode ? require('../magic') : require('./magic')
+const {Env} = mode ? require('./function/magic') : require('./function/magic')
 const $ = new Env('M集卡抽奖');
 $.lz = 'LZ_TOKEN_KEY=lztokef1eb8494b0af868bd18bdaf8;LZ_TOKEN_VALUE=Aa5RE8RuY4X3zA==;';
-
 $.activityUrl = process.env.M_WX_COLLECT_CARD_URL
     ? process.env.M_WX_COLLECT_CARD_URL
     : '';
@@ -107,6 +106,12 @@ $.logic = async function () {
     await api('crm/pageVisit/insertCrmPageVisit',
         `venderId=${$.venderId}&elementId=${encodeURIComponent(
             '邀请')}&pageId=${$.activityId}&pin=${encodeURIComponent($.Pin)}`);
+    
+      await api('wxCollectCard/drawCard',
+        `sourceId=${$.shareUuid}&activityId=${$.activityId}&type=1&pinImg=${encodeURIComponent(
+            $.attrTouXiang)}&pin=${encodeURIComponent(
+            $.Pin)}&jdNick=${encodeURIComponent(
+            $.nickname)}`);
     let saveSource = await api('wxCollectCard/saveSource',
         `activityId=${$.activityId}&pinImg=${encodeURIComponent(
             $.attrTouXiang)}&pin=${encodeURIComponent(
