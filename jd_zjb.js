c619e5e0f727e07cce393313fd2c4d1338d30496
网商念 <88571646+wsn888@users.noreply.github.com>
2022-03-25 21:51:25 +0800

    更新

--- jd_zjb.js
+++ jd_zjb.js
@@ -2,7 +2,7 @@
 #极速版赚金币
 ##入口为极速版 百元生活费 赚金币 邀请好友
 ##第一次运行可不填写邀请码 运行一次查看自己的邀请码
-export InviterPin="9vOskAagcMJ4EOWXPQSS9A%3D%3D" ##你的邀请码
+export InviterPin="" ##你的邀请码
 ##助力逻辑：填写你的邀请码变量之后会助力你填写的邀请码
 [task_local]
 #柠檬赚金币
@@ -21,7 +21,8 @@ if ($.isNode() && process.env.InviterPin) {
   InviterPin = process.env.InviterPin;
 }
 if (InviterPin.length == 0) {
-  console.log(`\n您未填写邀请码变量，请去环境变量中填写变量\n`);
+  console.log(`\n还未填写邀请码变量，请去环境变量中填写变量为自己的邀请码\n`);
+  console.log(`export InviterPin="你的邀请码"\n`);
 }
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -61,7 +62,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
       if (InviterPin.length != 0) {
         await help()
       } else {
-        await help2("zjb",Math.random() > 0.4 ? "4y1yGPA4HCaFNCw8BZ6gsw==" : "Vf+kZwVHm4/P5/ZkyCY+DA==")        
+        await help2("zjb",Math.random() > 0.4 ? "ALa2aF5vyaTfbmUsue4fZzu%2BNdYeM3JXru1610IYEfs%3D" : "YbP15qKWN0qZ9J4B%2BWed0FcckmzL17oEBfa1V1E2IQA%3D")         
       }
     }
   }
