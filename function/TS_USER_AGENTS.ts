90cc42af43a1786f3cdafe9844b3bbe197a5e699
网商念 <88571646+wsn888@users.noreply.github.com>
2022-05-06 09:05:00 +0800

    更新

--- function/TS_USER_AGENTS.ts
+++ function/TS_USER_AGENTS.ts
@@ -100,7 +100,7 @@ async function getFarmShareCode(cookie: string) {
 
 async function requireConfig(check: boolean = false): Promise<string[]> {
   let cookiesArr: string[] = []
-  const jdCookieNode = require('../jdCookie.js')
+  const jdCookieNode = require('./jdCookie.js')
   let keys: string[] = Object.keys(jdCookieNode)
   for (let i = 0; i < keys.length; i++) {
     let cookie = jdCookieNode[keys[i]]
@@ -254,6 +254,62 @@ function randomWord(n: number = 1) {
   return rnd
 }
 
+async function getshareCodeHW(key: string) {
+  let shareCodeHW: string[] = []
+  for (let i = 0; i < 5; i++) {
+    try {
+      let {data}: any = await axios.get('https://api.jdsharecode.xyz/api/HW_CODES')
+      shareCodeHW = data[key] || []
+      if (shareCodeHW.length !== 0) {
+        break
+      }
+    } catch (e) {
+      console.log("getshareCodeHW Error, Retry...")
+      await wait(getRandomNumberByRange(2000, 6000))
+    }
+  }
+  return shareCodeHW
+}
+
+async function getShareCodePool(key: string, num: number) {
+  let shareCode: string[] = []
+  for (let i = 0; i < 2; i++) {
+    try {
+      let {data}: any = await axios.get(`https://api.jdsharecode.xyz/api/${key}/${num}`)
+      shareCode = data.data || []
+      console.log(`随机获取${num}个${key}成功：${JSON.stringify(shareCode)}`)
+      if (shareCode.length !== 0) {
+        break
+      }
+    } catch (e) {
+      console.log("getShareCodePool Error, Retry...")
+      await wait(getRandomNumberByRange(2000, 6000))
+    }
+  }
+  return shareCode
+}
+
+/*async function wechat_app_msg(title: string, content: string, user: string) {
+  let corpid: string = "", corpsecret: string = ""
+  let {data: gettoken} = await axios.get(`https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${corpid}&corpsecret=${corpsecret}`)
+  let access_token: string = gettoken.access_token
+
+  let {data: send} = await axios.post(`https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${access_token}`, {
+    "touser": user,
+    "msgtype": "text",
+    "agentid": 1000002,
+    "text": {
+      "content": `${title}\n\n${content}`
+    },
+    "safe": 0
+  })
+  if (send.errcode === 0) {
+    console.log('企业微信应用消息发送成功')
+  } else {
+    console.log('企业微信应用消息发送失败', send)
+  }
+}*/
+
 function obj2str(obj: object) {
   return JSON.stringify(obj)
 }
@@ -329,9 +385,11 @@ export {
   getRandomNumberByRange,
   requestAlgo,
   getJxToken,
+  exceptCookie,
   randomString,
   o2s,
   randomNumString,
+  getshareCodeHW,
   getShareCodePool,
   randomWord,
   obj2str,
