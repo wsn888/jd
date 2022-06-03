549e4ebaad31a434decd03045ddd75d28584f505
网商念 <88571646+wsn888@users.noreply.github.com>
2022-06-03 14:45:36 +0800

    更新

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -1,5 +1,5 @@
 /*
-种豆得豆 脚本更新地址 jd_plantBean.js
+种豆得豆 脚本更新 jd_plantBean.js
 活动入口：京东APP我的-更多工具-种豆得豆
 已支持IOS京东多账号,云端多京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
@@ -27,7 +27,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //助力好友分享码(最多3个,否则后面的助力失败)
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes = []
+let shareCodes = ['']
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
