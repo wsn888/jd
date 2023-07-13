f3d8cd61c9f46b6cc13de529fc677f35cd129b46
网商念 <88571646+wsn888@users.noreply.github.com>
2023-07-13 09:26:12 +0800

    更新

--- jd_CheckCK.js
+++ jd_CheckCK.js
@@ -1,8 +1,8 @@
 /*
-cron "6 6 6 6 *" jd_CheckCK.js, tag:京东CK检测by-ccwav
+cron "30 * * * *" jd_CheckCK.js, tag:京东CK检测by-ccwav
  */
 //详细说明参考 https://github.com/ccwav/QLScript2.
-const $ = new Env('CK检测');
+const $ = new Env('京东CK检测');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -13,7 +13,7 @@ const {
     DisableCk,
     EnableCk,
     getstatus
-} = require('./function/ql');
+} = require('./ql');
 const api = got.extend({
         retry: {
             limit: 0
